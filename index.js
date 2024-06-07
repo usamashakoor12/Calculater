acdb7230c183b034791832909a0b388919d3b28a
usamashakoor12 <136337480+usamashakoor12@users.noreply.github.com>
2024-06-08 00:41:45 +0500

    calculater

---
+++ index.js
@@ -0,0 +1,37 @@
+function display(num) {
+  document.getElementById("output").value += num;
+}
+
+function solve() {
+  let result = document.getElementById("output").value;
+  let result2 = eval(result);
+  document.getElementById("output").value = result2;
+}
+
+function cls() {
+  document.getElementById("output").value = "";
+}
+
+function dlt() {
+  document.getElementById("output").value = document
+    .getElementById("output")
+    .value.slice(0, -1);
+}
+
+// function dlt() {
+//   document.cookie = document.getElementById("output").value;
+//   console.log(document.cookie);
+// }
+function ziro() {
+  document.getElementById("output").value = 0;
+}
+// function point(val) {
+//   document.getElementById("output").value = val;
+//   if (!val.includes(".")) {
+//     display.value += ".";
+//   }
+// }
+
+// document.onkeypress = (e) => {
+//   document.getElementById("demo").innerHTML = e.key;
+// };
